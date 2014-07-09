4b42c44b3bbac452b6b1eaa6ee2d4aeb512d8154
Matthew Flatt <mflatt@racket-lang.org>
2014-07-09 08:29:49 +0100

    declare package dependencies

--- info.rkt
+++ info.rkt
@@ -1,3 +1,11 @@
 #lang setup/infotab
 
 (define collection 'multi)
+
+(define deps '("algol60"
+               "base"
+               "draw-lib"
+               "gui-lib"
+               "parser-tools-lib"
+               "slideshow-lib"))
+(define build-deps '("scribble-lib"))
